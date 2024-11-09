a8054ea813af2d3e02b3e202cc445ebabcff486a
Joeyz141 <92345589+Joeyz141@users.noreply.github.com>
2024-11-08 23:59:17 -0800

    Add files via upload

---
+++ Services/IHolidaysApiService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HolidayApi.Models;
+
+namespace HolidayApi.Services
+{
+    public interface IHolidaysApiService
+    {
+        Task<List<HolidayModel>> GetHolidays(string countryCode, int year);
+    }
+}
+
+
+
